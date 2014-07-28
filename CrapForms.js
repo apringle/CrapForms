5985e377483eff28ff43b5bfc78e66e878e4ce62
SteveUpton <steve.j.upton@googlemail.com>
2014-07-28 22:08:12 +0100

    Wired functions to fire on click

--- CrapForms.js
+++ CrapForms.js
@@ -57,3 +57,11 @@ crapForms.restoreForms = function()
         }
     }
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+      document.getElementById('save-button').addEventListener('click', crapForms.saveForms);      
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('restore-button').addEventListener('click', crapForms.restoreForms);      
+});
