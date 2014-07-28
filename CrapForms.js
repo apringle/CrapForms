1ba73dfec1c4c5fc3e89c8a7f0fc0c07aec85e32
Alex Pringle <alex@alexpringle.co.uk>
2014-07-28 21:19:19 +0100

    added CrapForms.js

---
+++ CrapForms.js
@@ -0,0 +1,44 @@
+
+var crapForms = crapForms || {};
+
+crapForms.saveForms = function ()
+{
+    var inputs = document.querySelectorAll('input');
+    var inputValues = [];
+
+    for(var i = 0; i < inputs.length; i++)
+    {
+        var currentInput =  inputs[i];
+        inputValues.push(currentInput.value);
+    }
+
+    var crapFormsStorage;
+    if(localStorage['crapFormsStorage'])
+    {
+        crapFormsStorage = JSON.parse(localStorage['crapFormsStorage']);
+    }
+    else
+    {
+        crapFormsStorage = {};
+    }
+
+    crapFormsStorage[location.pathname] = inputValues;
+    localStorage['crapFormsStorage'] = JSON.stringify(crapFormsStorage);
+};
+
+crapForms.restoreForms = function()
+{
+    if(localStorage['crapFormsStorage'])
+    {
+        var crapFormsStorage = JSON.parse(localStorage['crapFormsStorage']);
+        var values = crapFormsStorage[location.pathname];
+        if(values)
+        {
+            var inputs = document.querySelectorAll('input');
+            for(var i = 0; i < inputs.length; i++)
+            {
+                inputs[i].value = values[i];
+            }
+        }
+    }
+}
