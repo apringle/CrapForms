cdb37e00e024bf3cb0c046a7de1d0bea254b057b
SteveUpton <steve.j.upton@googlemail.com>
2014-07-29 00:44:19 +0100

    Tidying senders

--- CrapFormsPopup.js
+++ CrapFormsPopup.js
@@ -1,21 +1,15 @@
-sendSaveMessage = function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var tab = tabs[0];
-    chrome.tabs.sendMessage(tab.id,{cmd: "save"});
-  });
-}
-
-sendRestoreMessage = function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var tab = tabs[0];
-    chrome.tabs.sendMessage(tab.id,{cmd: "restore"});
-  });
-}
-
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('save-button').addEventListener('click', sendSaveMessage);
+  document.getElementById('save-button').addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id,{cmd: "save"});
+    });
+  });
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('restore-button').addEventListener('click', sendRestoreMessage);
+  document.getElementById('restore-button').addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id,{cmd: "restore"});
+    });
+  });
 });
